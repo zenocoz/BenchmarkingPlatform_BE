85c1ee24ccac8ed38098dc71c36f07bde2fccd3d
Federico Soncini <federicosoncini@Federicos-Air.fritz.box>
2021-01-07 14:43:23 +0100

    cannot answer same question twice

--- src/services/exams/index.js
+++ src/services/exams/index.js
@@ -4,6 +4,7 @@ const uniqid = require("uniqid")
 const {join} = require("path")
 const {check, validationResult, matchedData} = require("express-validator")
 const {readDB, writeDB, randomizeArray} = require("../../fsUtilities")
+const {nextTick} = require("process")
 
 //INSTANCES
 const examsRouter = express.Router()
@@ -13,16 +14,16 @@ const questionsFolder = join(__dirname, "questions.json")
 const examsFolder = join(__dirname, "./exams.json")
 
 //Routes
-examsRouter.post("/start", async (req, res, err) => {
+examsRouter.post("/start", async (req, res, next) => {
   const questions = await readDB(questionsFolder)
   const randomArray = randomizeArray(questions)
   const randomQuestions = randomArray.slice(0, 4)
 
   const newExam = {
     ...req.body,
-    _id: uniqid(), // server generated
-    examDate: new Date(), // server generated
-    isCompleted: false, // false on creation
+    _id: uniqid(),
+    examDate: new Date(),
+    isCompleted: false,
     totalDuration: 30, // used only in extras
     questions: randomQuestions,
   }
@@ -32,7 +33,57 @@ examsRouter.post("/start", async (req, res, err) => {
 
   res.send(exams).sendStatus(201)
 })
-examsRouter.post("/id/answer", async (req, res, err) => {})
+examsRouter.post("/:id/answer", async (req, res, next) => {
+  //get exam
+
+  try {
+    const exams = await readDB(examsFolder)
+    const selectedExam = exams.find((exam) => exam._id === req.params.id)
+
+    if (selectedExam) {
+      if (!selectedExam.hasOwnProperty("score")) {
+        selectedExam.score = {
+          rightAnswers: 0,
+          wrongAnswers: 0,
+          answeredQuestions: [],
+        }
+      }
+      const questions = selectedExam.questions
+      let selectedQuestion = questions[req.body.question]
+
+      if (selectedQuestion) {
+        if (
+          selectedExam.score.answeredQuestions.indexOf(req.body.question) === -1
+        ) {
+          const answers = selectedQuestion.answers
+          const selectedAnswer = answers[req.body.answer]
+          selectedExam.score.answeredQuestions.push(req.body.question)
+          if (selectedAnswer.isCorrect === true) {
+            selectedExam.score.rightAnswers++
+          } else {
+            selectedExam.score.wrongAnswers++
+          }
+          await writeDB(examsFolder, exams)
+          res.send(exams)
+        } else {
+          const error = new Error("question already answered")
+          error.httpStatusCode = 403
+          next(error)
+        }
+      } else {
+        const error = new Error("question not found")
+        error.httStatusCode = 404
+        next(error)
+      }
+    } else {
+      const error = new Error("exam not found")
+      error.httpStatusCode = 404
+      next(error)
+    }
+  } catch (error) {
+    console.log(error)
+  }
+})
 examsRouter.get("/exams/id", async (req, res, err) => {})
 examsRouter.post("/start", async (req, res, err) => {})
 
