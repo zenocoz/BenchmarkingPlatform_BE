963468c89d8ebde160658d4bf2c7ab313d3c4f1d
Federico Soncini <federicosoncini@Federicos-Air.fritz.box>
2021-01-07 10:36:55 +0100

    minor changes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
 const express = require("express")
 const server = express()
 
-const port = "http://localhost:" + process.env.PORT || 3001
+const examsRoutes = require("./services/exams")
+
+//Routes
+
+//Use
+
+const port = process.env.PORT || 3001
 
 server.listen(() => console.log("server created on port", port))
